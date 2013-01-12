4212bfd45e1a23775e1bc0017d4a12fa18e6f82a
clacote <cyril@ninja-squad.com>
2013-01-12 04:24:03 +0100

    Useless optimisation

--- src/main/java/com/ninja_squad/codestory/scalaskel/ChangeComputer.java
+++ src/main/java/com/ninja_squad/codestory/scalaskel/ChangeComputer.java
@@ -9,33 +9,30 @@ import java.util.Set;
 public class ChangeComputer {
 
     public Set<Map<Unite, Integer>> change(final int sum) {
-        Map<Unite, Integer> current = Maps.newEnumMap(Unite.class);
-        return change(sum, current);
+        Set<Map<Unite, Integer>> results = Sets.newHashSet();
+        Map<Unite, Integer> initial = Maps.newEnumMap(Unite.class);
+        change(sum, initial, results);
+        return results;
     }
 
+    public void change(final int sum, Map<Unite, Integer> candidate, Set<Map<Unite, Integer>> results) {
 
-    public Set<Map<Unite, Integer>> change(final int sum, Map<Unite, Integer> candidate) {
-        Set<Map<Unite, Integer>> result = Sets.newHashSet();
+        if (sum == 0) {
+            results.add(candidate);
+        } else {
 
-        for (Unite unite : Unite.values()) {
-            int remaining = sum - unite.getValue();
-            if (remaining >= 0) {
-                // We could use one more of this unit
-                Map<Unite, Integer> nextCandidates = Maps.newHashMap(candidate);
-                int nb = candidate.get(unite) != null ? candidate.get(unite) : 0;
-                nextCandidates.put(unite, nb + 1);
+            for (Unite unite : Unite.values()) {
+                int remaining = sum - unite.getValue();
 
-                if (remaining == 0) {
-                    // Valid change
-                    result.add(nextCandidates);
-                } else {
-                    // Keep looking
-                    result.addAll(change(remaining, nextCandidates));
+                if (remaining >= 0) {
+                    // We could use one more of this unit
+                    Map<Unite, Integer> nextCandidates = Maps.newEnumMap(candidate);
+                    int nb = candidate.get(unite) != null ? candidate.get(unite) : 0;
+                    nextCandidates.put(unite, nb + 1);
+                    change(remaining, nextCandidates, results);
                 }
             }
         }
-
-        return result;
     }
 
     private static ChangeComputer ourInstance = new ChangeComputer();
